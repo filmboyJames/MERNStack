cd822c29807dc63ed403a06d01ed7c18ae68e4bc
filmboyJames <filmboynationwide@gmail.com>
2022-10-04 13:56:11 +0100

    More HTTP requests 4th Oct

--- httprequests.js
+++ httprequests.js
@@ -1,28 +1,36 @@
 'use strict'
 
-const url = "https://reqres.in/"
+const url = "https://reqres.in/api/users"
 
 
 
-fetch(url)
-    .then((response)=> {
-        if(response.status!==200){
-        console.error("Status: ",response.status);
-        return;
+// fetch(url)
+//     .then((response)=> {
+//         if(response.status!==200){
+//         console.error("Status: ",response.status);
+//         return;
+//     }
+//     response.json()
+//     .then(data=>console.info(data))
+// }).catch((err)=>console.error("err"));
+
+
+    axios.get(url)
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+
+
+document.getElementById("requestForm").addEventListener("submit", function(event) {
+   event.preventDefault();
+   console.log("Event: ", event);
+   const form = event.target;
+   console.log("FormL ",form);
+   const reqBody = {
+    first_name: form.input1.value,
+    email: form.input2.value,
     }
-    response.json()
-    .then(data=>console.info(data))
-}).catch((err)=>console.error("err"));
 
-// axios.get("url");
-//     .then(res => console.log(res))
-//     .catch(err => console.error(err));
-
-// const reqBody = {
-//     "information"
-// }
-
-// axios.post("url", reqBody)
-//     .then(res => console.log(res))
-//     .catch(err => console.error(err));
-
+axios.post(url, reqBody)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+});
