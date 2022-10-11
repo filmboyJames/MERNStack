284f07bd5a4252a78569c97e5206f2f2ae1b79ad
filmboyJames <filmboynationwide@gmail.com>
2022-10-11 15:24:36 +0100

    Testing 2

--- express/testing/tests/factorial.test.js
+++ express/testing/tests/factorial.test.js
@@ -6,6 +6,6 @@ describe("Factorial test", () => { //set of tests
         expect(factorial(120)).to.equal(5);
     })
     it(("should equal NONE"),() => {
-        expect(factorial(150)).to.equal("NONE");
+        expect(factorial(150)).to.equal("NOT A");
     })
 });
