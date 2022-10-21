109f08dd6ae9bf5abca96297e70929c7da527aaf
filmboyJames <filmboynationwide@gmail.com>
2022-10-21 11:23:47 +0100

    Selenium Testing Day 2 2

--- selenium/tests/react.test.js
+++ selenium/tests/react.test.js
@@ -5,7 +5,7 @@ const { expect } = require("chai");
 describe("jordan react tests", function () {
     this.timeout(40_000);
 
-it("Should convert miles to kilometres", async () => {
+it("Ex 1 Should convert miles to kilometres", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
         await driver.get("http://localhost:3000/testing/ex1");
@@ -19,7 +19,7 @@ it("Should convert miles to kilometres", async () => {
   })
 
 
-it("Should convert kilometres to miles", async () => {
+it("Ex 1 Should convert kilometres to miles", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
         await driver.get("http://localhost:3000/testing/ex1");
@@ -32,7 +32,7 @@ it("Should convert kilometres to miles", async () => {
     }
   })
 
-  it("Should display the counted number of clicks", async () => {
+  it("Ex 2 Should display the counted number of clicks", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
         await driver.get("http://localhost:3000/testing/ex2");
@@ -47,7 +47,25 @@ it("Should convert kilometres to miles", async () => {
     }
   })
 
-  it("Should display the searched for film", async () => {
+  it("Ex 2 Check the history after a number of clicks", async () => {
+    const driver = await new Builder().forBrowser("chrome").build();
+    try {
+        await driver.get("http://localhost:3000/testing/ex2");
+        const search = await driver.findElement(By.css("#root > div > div.container > div.input-group > button:nth-child(4)"))
+        for(let i=0; i<3; i++){
+            await search.click();
+        }
+        for (let j=1; j<4; j++){
+            const result = await driver.findElement(By.css(`#root > div > div.container > div:nth-child(5) > p:nth-child(${j})`));
+            expect(await result.getText()).to.equal(`${j}`);
+        }
+        
+    } finally {
+      await driver.quit();
+    }
+  })
+
+  it("Ex 3 Should display the searched for film", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
         await driver.get("http://localhost:3000/testing/ex3");
@@ -61,7 +79,7 @@ it("Should convert kilometres to miles", async () => {
     }
   })
 
-  it("Should display the searched for film", async () => {
+  it("Ex 3 Should display the searched for film after clickthrough", async () => {
     const driver = await new Builder().forBrowser("chrome").build();
     try {
         await driver.get("http://localhost:3000/testing/ex3");
@@ -69,7 +87,9 @@ it("Should convert kilometres to miles", async () => {
         await search.sendKeys("seven brides for seven brothers");
         await search.sendKeys(Key.ENTER);
         const result = await driver.wait(until.elementLocated(By.css("#root > div > div.container > div.container > div > div:nth-child(1) > div > div > div")));
-      expect(await result.getText()).to.equal("Seven Brides for Seven Brothers");
+        await result.click();
+        const result2 = await driver.wait(until.elementLocated(By.css("#root > div > div:nth-child(3) > div > div > div")));
+      expect(await result2.getText()).to.equal("Seven Brides for Seven Brothers");
     } finally {
       await driver.quit();
     }
